e7706ec5ae2ce7fc3c69dfd3e8a1a25a1f686d38
Alejandro Reyes <alejo_gnr15@hotmail.com>
2017-12-27 13:16:41 -0500

    Mostrar mapa en el fragment del mapa, sin geolocalización, se incorpora api de arcGis

--- app/src/main/java/com/esri/alejo/ramapa/fragmentMapa.java
+++ app/src/main/java/com/esri/alejo/ramapa/fragmentMapa.java
@@ -7,12 +7,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.MapView;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class fragmentMapa extends Fragment {
-
+    private MapView vistaMap;
+    private View view;
 
     public fragmentMapa() {
         // Required empty public constructor
@@ -22,8 +27,27 @@ public class fragmentMapa extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_mapa, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mapa, container, false);
+        crearMapa();
+        return view;
     }
 
+    public void crearMapa(){
+        vistaMap = view.findViewById(R.id.mapView);
+        ArcGISMap map = new ArcGISMap(Basemap.Type.TOPOGRAPHIC, 34.056295, -117.195800, 16);
+        vistaMap.setMap(map);
+    }
+
+    @Override
+    public void onPause() {
+        vistaMap.pause();
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        vistaMap.resume();
+    }
 }
