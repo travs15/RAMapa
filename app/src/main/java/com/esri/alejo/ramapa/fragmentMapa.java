4f4f005e9d40f8d336af83fdbad3069b1d8ceb2d
Alejandro Reyes <alejo_gnr15@hotmail.com>
2017-12-29 10:55:49 -0500

    Geolocalizacion de usuario con location display

--- app/src/main/java/com/esri/alejo/ramapa/fragmentMapa.java
+++ app/src/main/java/com/esri/alejo/ramapa/fragmentMapa.java
@@ -6,9 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.LocationDisplay;
 import com.esri.arcgisruntime.mapping.view.MapView;
 
 
@@ -17,7 +19,9 @@ import com.esri.arcgisruntime.mapping.view.MapView;
  */
 public class fragmentMapa extends Fragment implements View.OnClickListener {
     private MapView vistaMap;
-    private View view;
+    public View view;
+    //Uso para localizacion
+    public LocationDisplay locationDisplay;
 
     public fragmentMapa() {
         // Required empty public constructor
@@ -28,14 +32,17 @@ public class fragmentMapa extends Fragment implements View.OnClickListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_mapa, container, false);
+        vistaMap = view.findViewById(R.id.mapView);
+        locationDisplay = vistaMap.getLocationDisplay();
         // Inflate the layout for this fragment
         crearMapa();
+        geoLocalizacion();
         return view;
     }
 
+    //todo lo referente a crear la vista del mapa--------------------------------------------------
     public void crearMapa(){
-        vistaMap = view.findViewById(R.id.mapView);
-        ArcGISMap map = new ArcGISMap(Basemap.Type.TOPOGRAPHIC, 34.056295, -117.195800, 16);
+        ArcGISMap map = new ArcGISMap(Basemap.Type.TOPOGRAPHIC, 4.6097100,  -74.0817500, 16);
         vistaMap.setMap(map);
     }
 
@@ -51,6 +58,26 @@ public class fragmentMapa extends Fragment implements View.OnClickListener {
         vistaMap.resume();
     }
 
+    //geolocalizacion
+    //obtener punto de localizacion con gps
+    private void geoLocalizacion() {
+        try {
+            locationDisplay.addDataSourceStatusChangedListener(new LocationDisplay.DataSourceStatusChangedListener() {
+                @Override
+                public void onStatusChanged(LocationDisplay.DataSourceStatusChangedEvent dataSourceStatusChangedEvent) {
+
+                    if (dataSourceStatusChangedEvent.isStarted())
+                        return;
+
+                    if (dataSourceStatusChangedEvent.getError() == null)
+                        return;
+                }
+            });
+            locationDisplay.startAsync();
+        } catch (Exception e) {
+            Toast.makeText(view.getContext(), e.getMessage().toString(), Toast.LENGTH_LONG).show();
+        }
+    }
     @Override
     public void onClick(View v) {
 
